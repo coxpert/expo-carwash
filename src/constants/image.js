8a3599ed413de2394eef74e7b16cd2a1e7fb8478
bunbalhara <nanjingzhe425@gmail.com>
2020-11-11 01:31:50 +0800

    commit

---
+++ src/constants/image.js
@@ -0,0 +1,7 @@
+// images
+
+export const imgBackground = require('../assets/images/background.png');
+export const imgMenu1 = require('../assets/images/menu-1.png')
+export const imgMenu2 = require('../assets/images/menu-2.png')
+export const imgMenu3 = require('../assets/images/menu-3.png')
+export const imgLogoText = require('../assets/images/logo-text.png')
